12e0b4341f13502593e682d7e1eafb39a8cdc0d2
Osama Ahmad <osamafayyaz322@gmail.com>
2022-07-13 13:22:24 -0400

    build(api): built /weather api endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express');
 const hbs = require('hbs')
 const app = express();
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode')
 const port = 3000
 
 // Define paths for Express config
@@ -45,9 +47,23 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address'
         })
     }
-    res.send({
-        address: "Brampton"
-    });
+    geocode(req.query.address, (error, { latitude, longitude, location}) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location: location,
+                address: req.query.address
+            });
+        })
+        
+    })
+   
 })
 
 app.get('/products', (req, res) => {
